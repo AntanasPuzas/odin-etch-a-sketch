7da09e7e1266ba38619ea5ffaccadc87932e20a3
Antanas Pužas <antanaspuzas@protonmail.com>
2022-06-07 18:20:51 +0300

    Add event listener to change background color of squares on mouseover

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 const body = document.querySelector("body");
 
-
 for (let i = 0; i < 16; i++) {
     const divRow = document.createElement("div");
+    divRow.id = `row-${i}`;
     divRow.classList.add("row");
     for (let j = 0; j < 16; j++) {
         const square = document.createElement("div");
-        square.id = `${i}` + "-" + `${j}`;
+        square.id = `square-${i}-${j}`;
         square.classList.add("square");
+
         divRow.appendChild(square);
     }
     body.appendChild(divRow);
 }
+
+const squares = document.querySelectorAll(".square");
+squares.forEach(square => square.addEventListener("mouseover", () =>
+    square.classList.add("squareHover")));
